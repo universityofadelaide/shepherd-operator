6eb027971593b8d926fcb1c3ea02fe3ab97cb0ed
Nick Santamaria <nick.santamaria@previousnext.com.au>
2019-07-10 12:32:36 +1000

    Added cronexpr package.

--- pkg/controller/backupscheduled/backupscheduled_controller.go
+++ pkg/controller/backupscheduled/backupscheduled_controller.go
@@ -1,48 +1,39 @@
-/*
-Copyright 2019 University of Adelaide.
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
-
 package backupscheduled
 
 import (
 	"context"
-	"reflect"
+	"fmt"
+	"time"
 
-	extensionv1 "github.com/universityofadelaide/shepherd-operator/pkg/apis/extension/v1"
-	appsv1 "k8s.io/api/apps/v1"
+	"github.com/go-test/deep"
+	"github.com/pkg/errors"
+	"github.com/skpr/operator/pkg/utils/controller/logger"
+	v1 "github.com/universityofadelaide/shepherd-operator/pkg/apis/meta/v1"
+	"io/ioutil"
+	batchv1 "k8s.io/api/batch/v1"
 	corev1 "k8s.io/api/core/v1"
-	"k8s.io/apimachinery/pkg/api/errors"
+	kerrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 	"sigs.k8s.io/controller-runtime/pkg/handler"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
-	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/source"
+
+	extensionv1 "github.com/universityofadelaide/shepherd-operator/pkg/apis/extension/v1"
+	"github.com/universityofadelaide/shepherd-operator/pkg/utils/k8s/sync"
+	resticutils "github.com/universityofadelaide/shepherd-operator/pkg/utils/restic"
+	"github.com/universityofadelaide/shepherd-operator/pkg/apis/extension"
+	"github.com/gorhill/cronexpr"
 )
 
-var log = logf.Log.WithName("controller")
-
-/**
-* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
-* business logic.  Delete these comments after modifying this file.*
- */
+const ControllerName = "backup-scheduled-controller"
 
 // Add creates a new BackupScheduled Controller and adds it to the Manager with default RBAC. The Manager will set fields on the Controller
 // and Start it when the Manager is Started.
@@ -58,7 +49,7 @@ func newReconciler(mgr manager.Manager) reconcile.Reconciler {
 // add adds a new Controller to mgr with r as the reconcile.Reconciler
 func add(mgr manager.Manager, r reconcile.Reconciler) error {
 	// Create a new controller
-	c, err := controller.New("backupscheduled-controller", mgr, controller.Options{Reconciler: r})
+	c, err := controller.New(ControllerName, mgr, controller.Options{Reconciler: r})
 	if err != nil {
 		return err
 	}
@@ -69,12 +60,8 @@ func add(mgr manager.Manager, r reconcile.Reconciler) error {
 		return err
 	}
 
-	// TODO(user): Modify this to be the types you create
-	// Uncomment watch a Deployment created by BackupScheduled - change this for objects you create
-	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
-		IsController: true,
-		OwnerType:    &extensionv1.BackupScheduled{},
-	})
+	// Watch for changes to Backup
+	err = c.Watch(&source.Kind{Type: &extensionv1.Backup{}}, &handler.EnqueueRequestForObject{})
 	if err != nil {
 		return err
 	}
@@ -92,76 +79,135 @@ type ReconcileBackupScheduled struct {
 
 // Reconcile reads that state of the cluster for a BackupScheduled object and makes changes based on the state read
 // and what is in the BackupScheduled.Spec
-// TODO(user): Modify this Reconcile function to implement your Controller logic.  The scaffolding writes
-// a Deployment as an example
 // Automatically generate RBAC rules to allow the Controller to read and write Deployments
-// +kubebuilder:rbac:groups=apps,resources=deployments,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=apps,resources=deployments/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=extension.shepherd,resources=backups,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=extension.shepherd,resources=backups/status,verbs=get;update;patch
 // +kubebuilder:rbac:groups=extension.shepherd,resources=backupscheduleds,verbs=get;list;watch;create;update;patch;delete
 // +kubebuilder:rbac:groups=extension.shepherd,resources=backupscheduleds/status,verbs=get;update;patch
 func (r *ReconcileBackupScheduled) Reconcile(request reconcile.Request) (reconcile.Result, error) {
-	// Fetch the BackupScheduled instance
-	instance := &extensionv1.BackupScheduled{}
-	err := r.Get(context.TODO(), request.NamespacedName, instance)
+	log := logger.New(ControllerName, request.Namespace, request.Name)
+	log.Info("Starting reconcile loop")
+
+	backupScheduled := &extensionv1.BackupScheduled{}
+	err := r.Get(context.TODO(), request.NamespacedName, backupScheduled)
 	if err != nil {
-		if errors.IsNotFound(err) {
-			// Object not found, return.  Created objects are automatically garbage collected.
-			// For additional cleanup logic use finalizers.
+		if kerrors.IsNotFound(err) {
 			return reconcile.Result{}, nil
 		}
-		// Error reading the object - requeue the request.
+
 		return reconcile.Result{}, err
 	}
 
-	// TODO(user): Change this to be the object type created by your controller
-	// Define the desired Deployment object
-	deploy := &appsv1.Deployment{
+	if _, found := backupScheduled.ObjectMeta.GetLabels()["site"]; !found {
+		// @todo add some info to the status identifying the backup failed
+		log.Info(fmt.Sprintf("BackupScheduled %s doesn't have a site label, skipping.", backup.ObjectMeta.Name))
+		return reconcile.Result{}, nil
+	}
+
+	now := time.Now()
+
+	// Set up the first schedule if
+	if backupScheduled.Status.LastExecutedTime == nil {
+		next := cronexpr.MustParse(backupScheduled.Spec.Schedule).Next(now)
+		nextDuration := next.Unix() - now.Unix()
+		return reconcile.Result{
+			RequeueAfter: time.Duration(nextDuration),
+		}, nil
+	}
+
+
+
+
+	var backup extensionv1.Backup
+	backup.Name = fmt.Sprintf("%s-%d", backupScheduled.Name, now.Unix())
+	backup.Labels = backupScheduled.Labels
+	backup.Spec.MySQL = backupScheduled.Spec.MySQL
+	backup.Spec.Volumes = backupScheduled.Spec.Volumes
+
+
+	var params = resticutils.PodSpecParams{
+		CPU:         "100m",
+		Memory:      "512Mi",
+		ResticImage: "docker.io/restic/restic:0.9.5",
+		MySQLImage:  "docker.io/library/mariadb:10",
+		WorkingDir:  "/home/shepherd",
+		Tags:        []string{},
+	}
+	spec, err := resticutils.PodSpecBackup(backup, params, backup.ObjectMeta.GetLabels()["site"])
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	var (
+		parallelism    int32 = 1
+		completions    int32 = 1
+		activeDeadline int64 = 3600
+		backOffLimit   int32 = 2
+	)
+
+	job := &batchv1.Job{
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      instance.Name + "-deployment",
-			Namespace: instance.Namespace,
+			Name:      fmt.Sprintf("%s-%s", resticutils.Prefix, backup.ObjectMeta.Name),
+			Namespace: backup.ObjectMeta.Namespace,
 		},
-		Spec: appsv1.DeploymentSpec{
-			Selector: &metav1.LabelSelector{
-				MatchLabels: map[string]string{"deployment": instance.Name + "-deployment"},
-			},
+		Spec: batchv1.JobSpec{
+			Parallelism:           &parallelism,
+			Completions:           &completions,
+			ActiveDeadlineSeconds: &activeDeadline,
+			BackoffLimit:          &backOffLimit,
 			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{Labels: map[string]string{"deployment": instance.Name + "-deployment"}},
-				Spec: corev1.PodSpec{
-					Containers: []corev1.Container{
-						{
-							Name:  "nginx",
-							Image: "nginx",
-						},
-					},
-				},
+				Spec: spec,
 			},
 		},
 	}
-	if err := controllerutil.SetControllerReference(instance, deploy, r.scheme); err != nil {
+
+	log.Info("Syncing Job")
+	result, err := controllerutil.CreateOrUpdate(context.TODO(), r.Client, job, sync.Job(backup, r.scheme))
+	if err != nil {
 		return reconcile.Result{}, err
 	}
+	log.Infof("Synced Job %s with status: %s", job.ObjectMeta.Name, result)
 
-	// TODO(user): Change this for the object type created by your controller
-	// Check if the Deployment already exists
-	found := &appsv1.Deployment{}
-	err = r.Get(context.TODO(), types.NamespacedName{Name: deploy.Name, Namespace: deploy.Namespace}, found)
-	if err != nil && errors.IsNotFound(err) {
-		log.Info("Creating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
-		err = r.Create(context.TODO(), deploy)
-		return reconcile.Result{}, err
-	} else if err != nil {
-		return reconcile.Result{}, err
+	log.Info("Syncing status")
+	status := extensionv1.BackupStatus{
+		Phase:          v1.PhaseNew,
+		StartTime:      job.Status.StartTime,
+		CompletionTime: job.Status.CompletionTime,
 	}
 
-	// TODO(user): Change this for the object type created by your controller
-	// Update the found object and write the result back if there are any changes
-	if !reflect.DeepEqual(deploy.Spec, found.Spec) {
-		found.Spec = deploy.Spec
-		log.Info("Updating Deployment", "namespace", deploy.Namespace, "name", deploy.Name)
-		err = r.Update(context.TODO(), found)
-		if err != nil {
-			return reconcile.Result{}, err
+	if job.Status.Active > 0 {
+		status.Phase = v1.PhaseInProgress
+	} else {
+		if job.Status.Succeeded > 0 {
+			resticId, err := getResticIdFromJob(r.Config, job)
+			if err != nil {
+				return reconcile.Result{}, errors.Wrap(err, "failed to parse resticId")
+			}
+
+			if resticId != "" {
+				status.ResticID = resticId
+				status.Phase = v1.PhaseCompleted
+			} else {
+				status.Phase = v1.PhaseFailed
+			}
+		}
+		if job.Status.Failed > 0 {
+			status.Phase = v1.PhaseFailed
 		}
 	}
+
+	if diff := deep.Equal(backup.Status, status); diff != nil {
+		log.Info(fmt.Sprintf("Status change dectected: %s", diff))
+
+		backup.Status = status
+
+		err := r.Status().Update(context.TODO(), backup)
+		if err != nil {
+			return reconcile.Result{}, errors.Wrap(err, "failed to update status")
+		}
+	}
+
+	log.Info("Reconcile finished")
+
 	return reconcile.Result{}, nil
 }
