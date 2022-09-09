7228e4770f4855c471bdcc63366612bca000dda9
Nick Schuch <nick@previousnext.com.au>
2022-09-09 17:26:34 +1000

    Doesn't exist checks

--- controllers/extension/backup/controller.go
+++ controllers/extension/backup/controller.go
@@ -165,8 +165,8 @@ func (r *Reconciler) createSecret(ctx context.Context, backup *extensionv1.Backu
 	}
 
 	_, err := r.ClientSet.CoreV1().Secrets(secret.ObjectMeta.Namespace).Create(ctx, secret, metav1.CreateOptions{})
-	if kerrors.IsAlreadyExists(err) {
-		return nil
+	if err != nil && !kerrors.IsAlreadyExists(err) {
+		return err
 	}
 
 	return err
